af5fb5e0ffec87d59a7b0df778d226efc3132e57
Kitsune Ral <Kitsune-Ral@users.sf.net>
2016-04-11 10:44:46 +0900

    Minor fixes and cleanup
    1. Remove accidental trailing \
    2. Show the contents of NetworkError in logs

--- jobs/basejob.cpp
+++ jobs/basejob.cpp
@@ -81,7 +81,7 @@ void BaseJob::start()
     QNetworkRequest req = QNetworkRequest(url);
     req.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
-    req.setAttribute(QNetworkRequest::FollowRedirectsAttribute, true);\
+    req.setAttribute(QNetworkRequest::FollowRedirectsAttribute, true);
     req.setMaximumRedirectsAllowed(10);
 #endif
     QJsonDocument data = QJsonDocument(this->data());
@@ -124,7 +124,7 @@ void BaseJob::gotReply()
 {
     if( d->reply->error() != QNetworkReply::NoError )
     {
-        qDebug() << "NetworkError!!!";
+        qDebug() << "NetworkError!!!" << d->reply->error();
         fail( NetworkError, d->reply->errorString() );
         return;
     }
