879372e85ba8b6907f79ac129931bbf656697c24
Felix Rohrbach <fxrh@gmx.de>
2016-10-02 03:21:10 +0200

    Frist version of EncryptionManager
    For now, it only supports creating an account and reading the public
    part of the identity keys

---
+++ encryptionmanager.cpp
@@ -0,0 +1,99 @@
+/******************************************************************************
+ * Copyright (C) 2016 Felix Rohrbach <kde@fxrh.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ ******************************************************************************/
+
+
+#include "encryptionmanager.h"
+
+#include <olm/olm.h>
+
+#include <climits>
+#include <random>
+#include <algorithm>
+#include <functional>
+
+#include <QtCore/QDebug>
+
+using namespace QMatrixClient;
+
+using random_bytes_engine = std::independent_bits_engine<
+    std::default_random_engine, CHAR_BIT, unsigned char>;
+
+class EncryptionManager::Private
+{
+    public:
+        Private(QString id) : userId(id)
+                            , valid(false)
+                            , account(nullptr) {}
+
+        QString userId;
+        QString deviceId;
+        bool valid;
+
+        OlmAccount* account;
+        std::vector<unsigned char> publicIdentityKeys;
+
+        random_bytes_engine rbe;
+};
+
+
+EncryptionManager::EncryptionManager(QString userId)
+    : d(new Private(userId))
+{
+}
+
+bool EncryptionManager::isValid()
+{
+    return d->valid;
+}
+
+bool EncryptionManager::initialize(QString deviceId)
+{
+    d->deviceId = deviceId;
+
+    size_t randomSize = olm_create_account_random_length(d->account);
+    std::vector<unsigned char> randomData(randomSize);
+    std::generate(begin(randomData), end(randomData), std::ref(d->rbe));
+    size_t create_error = olm_create_account(d->account, (void*) &randomData[0], randomSize);
+
+    if( create_error == olm_error() )
+    {
+        qDebug() << "OLM ERROR: ACCOUNT: creating: " << olm_account_last_error(d->account);
+        return false;
+    }
+
+    qDebug() << "OLM: successfully created keys";
+
+    size_t publicIdentitySize = olm_account_identity_keys_length(d->account);
+    d->publicIdentityKeys.resize(publicIdentitySize, 0);
+    size_t readError = olm_account_identity_keys(d->account, (void*) &d->publicIdentityKeys[0], publicIdentitySize);
+    if( readError != olm_error() )
+    {
+        qDebug() << "OLM ERROR: ACCOUNT: reading keys: " << olm_account_last_error(d->account);
+        return false;
+    }
+
+    qDebug() << "OLM: successfully read keys";
+
+    d->valid = true;
+    return true;
+}
+
+const std::vector<unsigned char>& EncryptionManager::publicIdentityKeys() const
+{
+    return d->publicIdentityKeys;
+}
---
+++ encryptionmanager.h
@@ -0,0 +1,61 @@
+/******************************************************************************
+ * Copyright (C) 2016 Felix Rohrbach <kde@fxrh.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ ******************************************************************************/
+
+#ifndef QMATRIXCLIENT_ENCRYPTIONMANAGER_H
+#define QMATRIXCLIENT_ENCRYPTIONMANAGER_H
+
+#include <QtCore/QString>
+
+#include <vector>
+
+namespace QMatrixClient
+{
+    /**
+     * EncryptionManager manages the cryptographic operations for the
+     * E2E encryption of Matrix using libolm.
+     *
+     * An instance of EncryptionManager manages one user account.
+     */
+    class EncryptionManager
+    {
+        public:
+            EncryptionManager(QString userId);
+
+            /**
+             * Check whether this is a valid instace of EncryptionManager,
+             * ready to use.
+             */
+            bool isValid();
+
+            /**
+             * Set the device id and create all necessairy long term keys.
+             *
+             * If any error happens, this function returns false.
+             */
+            bool initialize(QString deviceId);
+
+            const std::vector<unsigned char>& publicIdentityKeys() const;
+
+        private:
+            class Private;
+            Private* d;
+    };
+}
+
+#endif // QMATRIXCLIENT_ENCRYPTIONMANAGER_H
+
