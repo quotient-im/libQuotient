8ae1da50f8474a698ce83f61233b3a8975faeaee
Kitsune Ral <Kitsune-Ral@users.sf.net>
2017-09-09 20:14:08 +0900

    First files made by api-generator
    Actual usage will come with the next commit.

---
+++ jobs/generated/inviting.h
@@ -0,0 +1,42 @@
+/******************************************************************************
+ * THIS FILE IS GENERATED - ANY EDITS WILL BE OVERWRITTEN
+ */
+
+
+#pragma once
+
+
+#include "../basejob.h"
+
+
+
+#include <QtCore/QString>
+
+
+
+
+namespace QMatrixClient
+{
+
+
+    // Operations
+    
+    /**
+     
+     */
+    class InviteUserJob : public BaseJob
+    {
+        public:
+            InviteUserJob(const ConnectionData* connection
+            
+                    , 
+                    QString roomId
+            
+                    , 
+                    QString user_id
+            );
+    
+    };
+    
+
+} // namespace QMatrixClient
