1cf67730a0880a520ae04bdf4ef61592daa9fe06
Kitsune Ral <Kitsune-Ral@users.sf.net>
2017-09-26 20:57:28 +0900

    Introduce EncryptionEvent class
    This allows to detect if a room has been encrypted (no room state, just
    an event as of yet). Closes #84.

---
+++ lib/events/encryptionevent.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rusakov on 26/09/2017.
+//
+
+#include "encryptionevent.h"
---
+++ lib/events/encryptionevent.h
@@ -0,0 +1,43 @@
+/******************************************************************************
+ * Copyright (C) 2017 Kitsune Ral <kitsune-ral@users.sf.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#pragma once
+
+#include "roomevent.h"
+
+namespace QMatrixClient
+{
+    class EncryptionEvent : public RoomEvent
+    {
+        public:
+            DEFINE_EVENT_TYPEID("m.room.encryption", EncryptionEvent)
+
+            explicit EncryptionEvent(const QJsonObject& obj)
+                : RoomEvent(typeId(), obj)
+                , _algorithm(contentJson()["algorithm"].toString())
+            { }
+
+            QString algorithm() const { return _algorithm; }
+
+        private:
+            QString _algorithm;
+    };
+    REGISTER_EVENT_TYPE(EncryptionEvent)
+    DEFINE_EVENTTYPE_ALIAS(Encryption, EncryptionEvent)
+}  // namespace QMatrixClient
+
