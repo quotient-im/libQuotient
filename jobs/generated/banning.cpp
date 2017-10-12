8800690c691dc9534fdb0f2d902862f816704d50
Kitsune Ral <Kitsune-Ral@users.sf.net>
2017-10-13 00:02:46 +0200

    Support banning and unbanning
    Closes #38. Also rearranged #includes

---
+++ jobs/generated/banning.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+ * THIS FILE IS GENERATED - ANY EDITS WILL BE OVERWRITTEN
+ */
+
+
+#include "banning.h"
+
+#include "jobs/converters.h"
+#include <QtCore/QStringBuilder>
+
+using namespace QMatrixClient;
+
+static const auto basePath = QStringLiteral("/_matrix/client/r0");
+
+BanJob::BanJob(QString roomId, QString user_id, QString reason)
+    : BaseJob(HttpVerb::Post, "BanJob",
+        basePath % "/rooms/" % roomId % "/ban",
+        Query { },
+        Data {
+            { "user_id", toJson(user_id) },
+            { "reason", toJson(reason) }
+        }
+    )
+{ }
+
+UnbanJob::UnbanJob(QString roomId, QString user_id)
+    : BaseJob(HttpVerb::Post, "UnbanJob",
+        basePath % "/rooms/" % roomId % "/unban",
+        Query { },
+        Data {
+            { "user_id", toJson(user_id) }
+        }
+    )
+{ }
+
