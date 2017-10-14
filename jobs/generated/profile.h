c2b84e41fba24f32270c24c0a6f679a1c4de0eea
Kitsune Ral <Kitsune-Ral@users.sf.net>
2017-10-14 09:41:41 +0200

    Actually added generated/profile.* files, needed to change display name

---
+++ jobs/generated/profile.h
@@ -0,0 +1,80 @@
+/******************************************************************************
+ * THIS FILE IS GENERATED - ANY EDITS WILL BE OVERWRITTEN
+ */
+
+
+#pragma once
+
+#include "../basejob.h"
+
+#include <QtCore/QString>
+
+
+namespace QMatrixClient
+{
+
+    // Operations
+
+    class SetDisplayNameJob : public BaseJob
+    {
+        public:
+            explicit SetDisplayNameJob(QString userId, QString displayname = {});
+
+    };
+    class GetDisplayNameJob : public BaseJob
+    {
+        public:
+            explicit GetDisplayNameJob(QString userId);
+
+            ~GetDisplayNameJob() override;
+
+            const QString& displayname() const;
+            
+        protected:
+            Status parseJson(const QJsonDocument& data) override;
+            
+        private:
+            class Private;
+            Private* d;
+    };
+    class SetAvatarUrlJob : public BaseJob
+    {
+        public:
+            explicit SetAvatarUrlJob(QString userId, QString avatar_url = {});
+
+    };
+    class GetAvatarUrlJob : public BaseJob
+    {
+        public:
+            explicit GetAvatarUrlJob(QString userId);
+
+            ~GetAvatarUrlJob() override;
+
+            const QString& avatar_url() const;
+            
+        protected:
+            Status parseJson(const QJsonDocument& data) override;
+            
+        private:
+            class Private;
+            Private* d;
+    };
+    class GetUserProfileJob : public BaseJob
+    {
+        public:
+            explicit GetUserProfileJob(QString userId);
+
+            ~GetUserProfileJob() override;
+
+            const QString& avatar_url() const;
+            const QString& displayname() const;
+            
+        protected:
+            Status parseJson(const QJsonDocument& data) override;
+            
+        private:
+            class Private;
+            Private* d;
+    };
+
+} // namespace QMatrixClient
