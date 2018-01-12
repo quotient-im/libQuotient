9e8e958b5d72b9671425b5bcd34fa016874b8677
Kitsune Ral <Kitsune-Ral@users.sf.net>
2018-01-12 22:04:28 +0900

    BaseJob: afterStart(), beforeAbandon(), up/downloadProgress()
    To support the upcoming DownloadFileJob

--- jobs/basejob.cpp
+++ jobs/basejob.cpp
@@ -216,15 +216,21 @@ void BaseJob::Private::sendRequest()
     }
 }
 
-void BaseJob::beforeStart(const ConnectionData* connData)
-{
-}
+void BaseJob::beforeStart(const ConnectionData*)
+{ }
+
+void BaseJob::afterStart(const ConnectionData*, QNetworkReply*)
+{ }
+
+void BaseJob::beforeAbandon(QNetworkReply*)
+{ }
 
 void BaseJob::start(const ConnectionData* connData)
 {
     d->connection = connData;
     beforeStart(connData);
     sendRequest();
+    afterStart(connData, d->reply.data());
 }
 
 void BaseJob::sendRequest()
@@ -238,6 +244,10 @@ void BaseJob::sendRequest()
     connect( d->reply.data(), &QNetworkReply::finished, this, &BaseJob::gotReply );
     if (d->reply->isRunning())
     {
+        connect( d->reply.data(), &QNetworkReply::uploadProgress,
+                 this, &BaseJob::uploadProgress);
+        connect( d->reply.data(), &QNetworkReply::downloadProgress,
+                 this, &BaseJob::downloadProgress);
         d->timer.start(getCurrentTimeout());
         qCDebug(d->logCat) << this << "request has been sent";
         emit started();
@@ -430,6 +440,7 @@ void BaseJob::setStatus(int code, QString message)
 
 void BaseJob::abandon()
 {
+    beforeAbandon(d->reply.data());
     this->disconnect();
     if (d->reply)
         d->reply->disconnect(this);
