db5d99c8b3f68b116fc55a4563bb66db4bf82cf8
Kitsune Ral <Kitsune-Ral@users.sf.net>
2018-01-30 16:17:03 +0900

    Fix compatibility with Qt before 5.10

--- user.cpp
+++ user.cpp
@@ -70,7 +70,7 @@ QString User::id() const
 
 bool User::isGuest() const
 {
-    Q_ASSERT(!d->userId.isEmpty() && d->userId.front() == '@');
+    Q_ASSERT(!d->userId.isEmpty() && d->userId.startsWith('@'));
     auto it = std::find_if_not(d->userId.begin() + 1, d->userId.end(),
                                std::mem_fn(&QChar::isDigit));
     Q_ASSERT(it == d->userId.end());
