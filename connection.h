aa3f747fc8480a8d58b41d447ecb7ca9b43c96e1
Roman Plášil <me@rplasil.name>
2018-02-01 15:32:45 +0800

    Move forgetRoom over to slots #166

--- connection.h
+++ connection.h
@@ -73,20 +73,6 @@ namespace QMatrixClient
             QHash<QPair<QString, bool>, Room*> roomMap() const;
             QMap<QString, User*> users() const;
 
-            /** Sends /forget to the server and also deletes room locally.
-             * This method is in Connection, not in Room, since it's a
-             * room lifecycle operation, and Connection is an acting room manager.
-             * It ensures that the local user is not a member of a room (running /leave,
-             * if necessary) then issues a /forget request and if that one doesn't fail
-             * deletion of the local Room object is ensured.
-             * \param id - the room id to forget
-             * \return - the ongoing /forget request to the server; note that the
-             * success() signal of this request is connected to deleteLater()
-             * of a respective room so by the moment this finishes, there might be no
-             * Room object anymore.
-             */
-            ForgetRoomJob* forgetRoom(const QString& id);
-
             // FIXME: Convert Q_INVOKABLEs to Q_PROPERTIES
             // (breaks back-compatibility)
             QUrl homeserver() const;
@@ -224,6 +210,20 @@ namespace QMatrixClient
                 const QString& topic = {}, const QString& name = {});
 
             virtual JoinRoomJob* joinRoom(const QString& roomAlias);
+
+            /** Sends /forget to the server and also deletes room locally.
+             * This method is in Connection, not in Room, since it's a
+             * room lifecycle operation, and Connection is an acting room manager.
+             * It ensures that the local user is not a member of a room (running /leave,
+             * if necessary) then issues a /forget request and if that one doesn't fail
+             * deletion of the local Room object is ensured.
+             * \param id - the room id to forget
+             * \return - the ongoing /forget request to the server; note that the
+             * success() signal of this request is connected to deleteLater()
+             * of a respective room so by the moment this finishes, there might be no
+             * Room object anymore.
+             */
+            ForgetRoomJob* forgetRoom(const QString& id);
 
             // Old API that will be abolished any time soon. DO NOT USE.
 
