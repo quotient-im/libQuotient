dce6f26a125f237471c579675ac149a33a951d84
Kitsune Ral <Kitsune-Ral@users.sf.net>
2018-02-27 20:22:38 +0900

    jobs/generated: GetRoomTagsJob, SetRoomTagJob, DeleteRoomTagJob

---
+++ jobs/generated/tags.h
@@ -0,0 +1,57 @@
+/******************************************************************************
+ * THIS FILE IS GENERATED - ANY EDITS WILL BE OVERWRITTEN
+ */
+
+#pragma once
+
+#include "../basejob.h"
+
+#include <QtCore/QJsonObject>
+
+
+namespace QMatrixClient
+{
+    // Operations
+
+    class GetRoomTagsJob : public BaseJob
+    {
+        public:
+            /** Construct a URL out of baseUrl and usual parameters passed to
+             * GetRoomTagsJob. This function can be used when
+             * a URL for GetRoomTagsJob is necessary but the job
+             * itself isn't.
+             */
+            static QUrl makeRequestUrl(QUrl baseUrl, const QString& userId, const QString& roomId);
+
+            explicit GetRoomTagsJob(const QString& userId, const QString& roomId);
+            ~GetRoomTagsJob() override;
+
+            const QJsonObject& tags() const;
+
+        protected:
+            Status parseJson(const QJsonDocument& data) override;
+
+        private:
+            class Private;
+            QScopedPointer<Private> d;
+    };
+
+    class SetRoomTagJob : public BaseJob
+    {
+        public:
+            explicit SetRoomTagJob(const QString& userId, const QString& roomId, const QString& tag, const QJsonObject& body = {});
+    };
+
+    class DeleteRoomTagJob : public BaseJob
+    {
+        public:
+            /** Construct a URL out of baseUrl and usual parameters passed to
+             * DeleteRoomTagJob. This function can be used when
+             * a URL for DeleteRoomTagJob is necessary but the job
+             * itself isn't.
+             */
+            static QUrl makeRequestUrl(QUrl baseUrl, const QString& userId, const QString& roomId, const QString& tag);
+
+            explicit DeleteRoomTagJob(const QString& userId, const QString& roomId, const QString& tag);
+    };
+} // namespace QMatrixClient
