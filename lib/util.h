1de8d511251163ed35e0647c70c3e94e071b2fe0
Kitsune Ral <Kitsune-Ral@users.sf.net>
2018-12-06 19:12:28 +0900

    Special-case FALLTHROUGH for Clang

--- lib/util.h
+++ lib/util.h
@@ -31,6 +31,8 @@
 #define FALLTHROUGH [[fallthrough]]
 #elif __has_cpp_attribute(clang::fallthrough)
 #define FALLTHROUGH [[clang::fallthrough]]
+#elif __has_cpp_attribute(gnu::fallthrough)
+#define FALLTHROUGH [[gnu::fallthrough]]
 #else
 #define FALLTHROUGH // -fallthrough
 #endif
