ee1d26586572d4d74105a0713d0237dbc2d183f0
Kitsune Ral <Kitsune-Ral@users.sf.net>
2019-02-09 19:37:12 +0900

    csapi: add RedirectToSSOJob
    This is actually a rehash (MSC1721) of redirectToCAS that existed before but was explicitly disabled in the library because of its seeming uselessness in the context of non-web clients. On the second thought, however, `RedirectToSSOJob::makeRequestURL()` can actually be used to open a web browser from a non-web client in order to perform the login procedure.

---
+++ lib/csapi/sso_login_redirect.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+ * THIS FILE IS GENERATED - ANY EDITS WILL BE OVERWRITTEN
+ */
+
+#include "sso_login_redirect.h"
+
+#include "converters.h"
+
+#include <QtCore/QStringBuilder>
+
+using namespace QMatrixClient;
+
+static const auto basePath = QStringLiteral("/_matrix/client/r0");
+
+BaseJob::Query queryToRedirectToSSO(const QString& redirectUrl)
+{
+    BaseJob::Query _q;
+    addParam<>(_q, QStringLiteral("redirectUrl"), redirectUrl);
+    return _q;
+}
+
+QUrl RedirectToSSOJob::makeRequestUrl(QUrl baseUrl, const QString& redirectUrl)
+{
+    return BaseJob::makeRequestUrl(std::move(baseUrl),
+            basePath % "/login/sso/redirect",
+            queryToRedirectToSSO(redirectUrl));
+}
+
+static const auto RedirectToSSOJobName = QStringLiteral("RedirectToSSOJob");
+
+RedirectToSSOJob::RedirectToSSOJob(const QString& redirectUrl)
+    : BaseJob(HttpVerb::Get, RedirectToSSOJobName,
+        basePath % "/login/sso/redirect",
+        queryToRedirectToSSO(redirectUrl),
+        {}, false)
+{
+}
+
