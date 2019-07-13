f1546e894b2a7550bce3e8d84067968bf5cf7087
Alexey Andreyev <aa13q@ya.ru>
2019-07-14 02:33:39 +0300

    E2EE: provide a shared header with encryption standard key names

---
+++ lib/e2ee.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <QtCore/QStringList>
+
+namespace QMatrixClient
+{
+    static const auto AlgorithmKeyL = "algorithm"_ls;
+    static const auto RotationPeriodMsKeyL = "rotation_period_ms"_ls;
+    static const auto RotationPeriodMsgsKeyL = "rotation_period_msgs"_ls;
+
+    static const auto AlgorithmKey = QStringLiteral("algorithm");
+    static const auto RotationPeriodMsKey = QStringLiteral("rotation_period_ms");
+    static const auto RotationPeriodMsgsKey = QStringLiteral("rotation_period_msgs");
+
+    static const auto Ed25519Key = QStringLiteral("ed25519");
+    static const auto Curve25519Key = QStringLiteral("curve25519");
+    static const auto SignedCurve25519Key = QStringLiteral("signed_curve25519");
+    static const auto OlmV1Curve25519AesSha2AlgoKey = QStringLiteral("m.olm.v1.curve25519-aes-sha2");
+    static const auto MegolmV1AesSha2AlgoKey = QStringLiteral("m.megolm.v1.aes-sha2");
+    static const QStringList SupportedAlgorithms = { OlmV1Curve25519AesSha2AlgoKey, MegolmV1AesSha2AlgoKey };
+}  // namespace QMatrixClient
