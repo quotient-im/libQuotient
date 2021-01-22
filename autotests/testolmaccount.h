d9286b1ad5516082bc9b40adaceb9485acf4a553
Carl Schwan <carl@carlschwan.eu>
2021-01-22 23:38:32 +0100

    Add tests

---
+++ autotests/testolmaccount.h
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2021 Carl Schwan <carlschwan@kde.org>
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#ifdef Quotient_E2EE_ENABLED
+#include <QtTest/QtTest>
+
+class TestOlmAccount : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void pickleUnpickedTest();
+    void identityKeysValid();
+    void signatureValid();
+    void oneTimeKeysValid();
+    //void removeOneTimeKeys();
+};
+#endif
