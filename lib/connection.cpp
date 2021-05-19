12b5361ee1a669725dfa28f83e4ccf0fe4168219
Tobias Fella <fella@posteo.de>
2021-05-19 22:25:50 +0200

    Update tracked users list when new user joins encrypted room

--- lib/connection.cpp
+++ lib/connection.cpp
@@ -1862,7 +1862,7 @@ void Connection::Private::loadOutdatedUserDevices()
     });
 }
 
-void Connection::newEncryptedRoom(Room *room)
+void Connection::encryptionUpdate(Room *room)
 {
     for(const auto &user : room->users()) {
         if(!d->trackedUsers.contains(user->id())) {
