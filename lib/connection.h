4ec16bcfa825f3a23f087e00fe14d00a4ca48675
Tobias Fella <fella@posteo.de>
2021-05-19 22:25:50 +0200

    Update tracked users list when new user joins encrypted room

--- lib/connection.h
+++ lib/connection.h
@@ -667,7 +667,7 @@ public Q_SLOTS:
      */
     virtual PostReceiptJob* postReceipt(Room* room, RoomEvent* event);
 
-    void newEncryptedRoom(Room *room);
+    void encryptionUpdate(Room *room);
 
 Q_SIGNALS:
     /**
