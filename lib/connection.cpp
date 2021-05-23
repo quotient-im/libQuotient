68d39d339d1c4c3e41814b0a0beb2be69b09118d
Carl Schwan <carl@carlschwan.eu>
2021-05-24 00:40:30 +0200

    Uncomment some stuff

--- lib/connection.cpp
+++ lib/connection.cpp
@@ -107,7 +107,7 @@ public:
 
 #ifdef Quotient_E2EE_ENABLED
     std::unique_ptr<QOlmAccount> olmAccount;
-    //QScopedPointer<EncryptionManager> encryptionManager;
+    QScopedPointer<EncryptionManager> encryptionManager;
 #endif // Quotient_E2EE_ENABLED
 
     QPointer<GetWellknownJob> resolverJob = nullptr;
@@ -184,7 +184,6 @@ public:
     {
         qCWarning(E2EE) << "End-to-end encryption (E2EE) support is turned off.";
         return {};
-        /*
 #ifndef Quotient_E2EE_ENABLED
         qCWarning(E2EE) << "End-to-end encryption (E2EE) support is turned off.";
         return {};
@@ -244,7 +243,6 @@ public:
 
         return std::move(decryptedEvent);
 #endif // Quotient_E2EE_ENABLED
-*/
     }
 };
 
@@ -759,7 +757,6 @@ void Connection::Private::consumePresenceData(Events&& presenceData)
 
 void Connection::Private::consumeToDeviceEvents(Events&& toDeviceEvents)
 {
-/*
 #ifdef Quotient_E2EE_ENABLED
     // handling m.room_key to-device encrypted event
     visitEach(toDeviceEvents, [this](const EncryptedEvent& ee) {
@@ -769,20 +766,15 @@ void Connection::Private::consumeToDeviceEvents(Events&& toDeviceEvents)
             return;
         }
 
-        // TODO: full maintaining of the device keys
-        // with device_lists sync extention and /keys/query
-        qCDebug(E2EE) << "Getting device keys for the m.room_key sender:"
-                      << ee.senderId();
-        // encryptionManager->updateDeviceKeys();
-
         visit(*sessionDecryptMessage(ee),
             [this, senderKey = ee.senderKey()](const RoomKeyEvent& roomKeyEvent) {
-                if (auto* detectedRoom = q->room(roomKeyEvent.roomId()))
+                if (auto* detectedRoom = q->room(roomKeyEvent.roomId())) {
                     detectedRoom->handleRoomKeyEvent(roomKeyEvent, senderKey);
-                else
+                } else {
                     qCDebug(E2EE)
                         << "Encrypted event room id" << roomKeyEvent.roomId()
                         << "is not found at the connection" << q->objectName();
+                }
             },
             [](const Event& evt) {
                 qCDebug(E2EE) << "Skipping encrypted to_device event, type"
@@ -790,7 +782,6 @@ void Connection::Private::consumeToDeviceEvents(Events&& toDeviceEvents)
             });
     });
 #endif
-*/
 }
 
 void Connection::stopSync()
