f89c05408bc9a4bf59366f92c50f055d527b9a28
Tobias Fella <fella@posteo.de>
2021-06-12 18:58:54 +0200

    Remove unrelated changes

--- lib/encryptionmanager.h
+++ lib/encryptionmanager.h
@@ -19,7 +19,6 @@ class EncryptionManager : public QObject {
 public:
     explicit EncryptionManager(QObject* parent = nullptr);
     ~EncryptionManager();
-
     QString sessionDecryptMessage(const QJsonObject& personalCipherObject,
                                   const QByteArray& senderKey, std::unique_ptr<QOlmAccount>& account);
 
