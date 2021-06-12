ec954b2d56744e82017a04ac98bb5296c54d8905
Tobias Fella <fella@posteo.de>
2021-06-12 18:49:56 +0200

    Fix

--- lib/connection.cpp
+++ lib/connection.cpp
@@ -117,7 +117,6 @@ public:
     QVector<GetLoginFlowsJob::LoginFlow> loginFlows;
 
 #ifdef Quotient_E2EE_ENABLED
-<<<<<<< HEAD
     std::unique_ptr<QOlmAccount> olmAccount;
     bool isUploadingKeys = false;
     EncryptionManager *encryptionManager;
