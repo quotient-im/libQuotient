0a1b8b9f38ddadb32d54884a05f0e7246d7c25e7
Alexey Rusakov <Kitsune-Ral@users.sf.net>
2022-02-18 14:14:15 +0100

    run-tests.sh: use a trap for cleanup
    This both is more reliable (GHA executes scripts in fail-fast mode) and
    ensures that the return value is that of ctest.

--- autotests/run-tests.sh
+++ autotests/run-tests.sh
@@ -4,11 +4,13 @@ docker run -v `pwd`/data:/data --rm \
     -e SYNAPSE_SERVER_NAME=localhost -e SYNAPSE_REPORT_STATS=no matrixdotorg/synapse:v1.24.0 generate
 ./.ci/adjust-config.sh
 docker run -d \
-	--name synapse \
-	-p 1234:8008 \
-	-p 8448:8008 \
-	-p 8008:8008 \
-	-v `pwd`/data:/data matrixdotorg/synapse:v1.24.0
+    --name synapse \
+    -p 1234:8008 \
+    -p 8448:8008 \
+    -p 8008:8008 \
+    -v `pwd`/data:/data matrixdotorg/synapse:v1.24.0
+trap "rm -rf ./data/*; docker rm -f synapse 2>&1 >/dev/null; trap - EXIT" EXIT
+
 echo Waiting for synapse to start...
 until curl -s -f -k https://localhost:1234/_matrix/client/versions; do echo "Checking ..."; sleep 2; done
 echo Register alice
@@ -19,5 +21,3 @@ echo Register carl
 docker exec synapse /bin/sh -c 'register_new_matrix_user --admin -u carl -p secret -c /data/homeserver.yaml https://localhost:8008'
 
 GTEST_COLOR=1 ctest --verbose "$@"
-rm -rf ./data/*
-docker rm -f synapse 2>&1>/dev/null
