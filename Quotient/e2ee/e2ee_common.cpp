564773dcc36a9cc3e6a365c0ed45d552dd86d952
Anna “CyberTailor” <cyber@sysrq.in>
2023-05-10 00:02:05 +0500

    Fix E2EE build with LibreSSL

--- Quotient/e2ee/e2ee_common.cpp
+++ Quotient/e2ee/e2ee_common.cpp
@@ -25,6 +25,7 @@ QByteArray Quotient::byteArrayForOlm(size_t bufferSize)
 
 auto initializeSecureHeap()
 {
+#if !defined(LIBRESSL_VERSION_NUMBER)
     const auto result =
         CRYPTO_secure_malloc_init(FixedBufferBase::TotalSecureHeapSize, 16);
     if (result > 0) {
@@ -40,11 +41,16 @@ auto initializeSecureHeap()
     } else
         qCritical(E2EE) << "Secure heap could not be initialised, sensitive "
                            "data will remain in common dynamic memory";
+#else
+    const auto result = 0;
+    qCritical(E2EE) << "Secure heap is not available in LibreSSL";
+#endif
     return result;
 }
 
 uint8_t* allocate(size_t bytes, bool initWithZeros = false)
 {
+#if !defined(LIBRESSL_VERSION_NUMBER)
     static auto secureHeapInitialised [[maybe_unused]] = initializeSecureHeap();
 
     const auto p = static_cast<uint8_t*>(initWithZeros
@@ -55,6 +61,11 @@ uint8_t* allocate(size_t bytes, bool initWithZeros = false)
                  << "bytes of secure heap (requested" << bytes << "bytes),"
                  << CRYPTO_secure_used()
                  << "/ 65536 bytes of secure heap used in total";
+#else
+    const auto p = static_cast<uint8_t*>(initWithZeros
+                                             ? calloc(bytes, 1)
+                                             : malloc(bytes));
+#endif
     return p;
 }
 
@@ -114,10 +125,14 @@ void FixedBufferBase::clear()
     if (empty())
         return;
 
+#if !defined(LIBRESSL_VERSION_NUMBER)
     Q_ASSERT(CRYPTO_secure_allocated(data_));
     const auto actualSize = OPENSSL_secure_actual_size(data_);
     OPENSSL_secure_clear_free(data_, size_);
-    data_ = nullptr;
     qDebug(E2EE) << "Deallocated" << actualSize << "bytes,"
                  << CRYPTO_secure_used() << "/ 65536 bytes of secure heap used";
+#else
+    free(data_);
+#endif
+    data_ = nullptr;
 }
