fb72cadc4494f1e5659a5975f3d89fdb4176bb7d
Alexey Rusakov <Kitsune-Ral@users.sf.net>
2023-07-03 13:26:44 +0200

    SingleKeyValue: Use parentheses instead of braces
    Braces may trigger unintended list-initialisation with some types _and_
    compilers. E.g., QJsonArray has a constructor accepting a braced list
    of elements and GCC prefers it when it encounters
    `QJsonArray{ /*QJsonArray variable*/ }`, list-initialising the (outer)
    array with the passed variable implicitly wrapped in a QJsonValue;
    Clang prefers a copy/move constructor instead. The mess stems from
    a defect in the C++ standard itself, no less:
    https://cplusplus.github.io/CWG/issues/2137.html
    
    Using parentheses instead is not perfect either, as it allows
    narrowing casts for types like unsigned int (fortunately, that's not -
    doesn't seem to be - a problem for our codebase). Sometimes implicit
    casting really bites your feet...

--- Quotient/events/single_key_value.h
+++ Quotient/events/single_key_value.h
@@ -7,14 +7,9 @@ namespace Quotient {
 namespace EventContent {
     template <typename T, const QLatin1String& KeyStr>
     struct SingleKeyValue {
-        // NOLINTBEGIN(google-explicit-constructor): that check should learn
-        //                                           about explicit(false)
-        QUO_IMPLICIT SingleKeyValue(const T& v = {})
-            : value { v }
-        {}
-        QUO_IMPLICIT SingleKeyValue(T&& v)
-            : value { std::move(v) }
-        {}
+        // NOLINTBEGIN(google-explicit-constructor): that check should learn about explicit(false)
+        QUO_IMPLICIT SingleKeyValue(const T& v = {}) : value(v) {}
+        QUO_IMPLICIT SingleKeyValue(T&& v) : value(std::move(v)) {}
         // NOLINTEND(google-explicit-constructor)
         T value;
     };
@@ -25,7 +20,7 @@ struct JsonConverter<EventContent::SingleKeyValue<ValueT, KeyStr>> {
     using content_type = EventContent::SingleKeyValue<ValueT, KeyStr>;
     static content_type load(const QJsonValue& jv)
     {
-        return { fromJson<ValueT>(jv.toObject().value(JsonKey)) };
+        return fromJson<ValueT>(jv.toObject().value(JsonKey));
     }
     static QJsonObject dump(const content_type& c)
     {
