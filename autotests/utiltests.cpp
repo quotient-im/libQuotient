42b888e40f9ec8e24dae857f098120a421176513
Alexey Rusakov <Kitsune-Ral@users.sf.net>
2023-08-12 18:26:01 +0200

    utiltests: Add some tests for linkifyUrls()
    Including one for #691 (it's broken now, just as the issue describes).

--- autotests/utiltests.cpp
+++ autotests/utiltests.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 #include <Quotient/omittable.h>
+#include <Quotient/util.h>
 
 #include <QtTest/QtTest>
 
@@ -38,8 +39,34 @@ static_assert(std::is_same_v<Omittable<bool>,
 class TestUtils : public QObject {
     Q_OBJECT
 private Q_SLOTS:
-    // TODO
+    void testLinkifyUrl();
+
+private:
+    QString linkified(QString toLinkify) const
+    {
+        linkifyUrls(toLinkify);
+        return toLinkify;
+    }
 };
+
+#ifndef Q_MOC_RUN // moc chokes on the code below, doesn't really need it
+void TestUtils::testLinkifyUrl()
+{
+    QCOMPARE(
+        linkified("mailto:example@example.com"_ls),
+        "<a href='mailto:example@example.com'>mailto:example@example.com</a>"_ls);
+    QCOMPARE(
+        linkified("example@example.com"_ls),
+        "<a href='mailto:example@example.com'>example@example.com</a>"_ls);
+    QCOMPARE(linkified("https://example.com"_ls),
+             "<a href='https://example.com'>https://example.com</a>"_ls);
+    QCOMPARE(linkified("www.example.com"_ls),
+             "<a href='www.example.com'>www.example.com</a>"_ls);
+    QCOMPARE(linkified("www.example.com?email@example.com"_ls),
+             "<a href='www.example.com?email@example.com'>"
+             "www.example.com?email@example.com</a>"_ls);
+}
+#endif
 
 QTEST_APPLESS_MAIN(TestUtils)
 #include "utiltests.moc"
