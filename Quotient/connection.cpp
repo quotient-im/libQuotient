09b52908b8f9186114494147d2e4bbc601f19343
Tobias Fella <fella@posteo.de>
2023-10-19 21:57:49 +0200

    Remove Qt5 compatibility ifdefs

--- Quotient/connection.cpp
+++ Quotient/connection.cpp
@@ -42,11 +42,7 @@
     #include "e2ee/qolminboundsession.h"
 #endif // Quotient_E2EE_ENABLED
 
-#if QT_VERSION_MAJOR >= 6
-    #include <qt6keychain/keychain.h>
-#else
-    #include <qt5keychain/keychain.h>
-#endif
+#include <qt6keychain/keychain.h>
 
 #include <QtCore/QCoreApplication>
 #include <QtCore/QDir>
