368c8ee8c949d12155f5d0f146bfcb30611034c3
Joshua Goins <josh@redstrate.com>
2023-10-19 15:29:36 -0400

    Add blurhash implementation to Quotient
    So every client that wants to implement blurhashes doesn't have to copy
    this file over and over.

---
+++ Quotient/blurhash.h
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: 2024 Joshua Goins <josh@redstrate.com>
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "quotient_export.h"
+
+#include <QtGui/QImage>
+
+namespace Quotient {
+    /**
+     * @brief Encodes and decodes image to and from the BlurHash format. See https://blurha.sh/.
+     */
+    namespace BlurHash {
+        /** Decodes the @p blurhash string creating an image of @p size.
+          * @note Returns a null image if decoding failed.
+          */
+        QUOTIENT_API QImage decode(const QString &blurhash, const QSize &size);
+
+        /** Encodes the @p image and returns a blurhash string.
+         * @param image A non-null image.
+         * @param componentsX the number of components X-wise. Must be between 1 and 9.
+         * @param componentsY the number of components Y-wise. Must be between 1 and 9.
+         * @note Returns an empty string if it failed to encode the image.
+         */
+        QUOTIENT_API QString encode(const QImage &image, int componentsX = 4, int componentsY = 4);
+    }// namespace BlurHash
+} // namespace Quotient
