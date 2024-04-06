quotient-im/libQuotient

commit 4a93e124974e25842f2fe44d66b67796d1ac6a82

Emit a dedicated signal when all history loaded
Fixes #703.

Quotient/room.cpp
@@ -274,7 +274,7 @@ public:
     }
 
     Changes addNewMessageEvents(RoomEvents&& events);
-    void addHistoricalMessageEvents(RoomEvents&& events);
+    std::pair<Changes, rev_iter_t> addHistoricalMessageEvents(RoomEvents&& events);
 
     Changes updateStatsFromSyncData(const SyncRoomData &data, bool fromCache);
     void postprocessChanges(Changes changes, bool saveState = true);
@@ -2565,7 +2565,14 @@ void Room::Private::getPreviousContent(int limit, const QString& filter)
             prevBatch.reset();
         }
 
-        addHistoricalMessageEvents(eventsHistoryJob->chunk());
+        auto [changes, from] = addHistoricalMessageEvents(eventsHistoryJob->chunk());
+        // The following condition will only trigger once, next time getPreviousContent()
+        // will return without spawning GetRoomEventsJob
+        if (!prevBatch)
+            emit q->allHistoryLoadedChanged();
+        changes |= updateStats(from, historyEdge());
+        if (changes != Change::None)
+            postprocessChanges(changes);
     });
     connect(eventsHistoryJob, &QObject::destroyed, q,
             &Room::eventsHistoryJobChanged);
@@ -3147,14 +3154,14 @@ Room::Changes Room::Private::addNewMessageEvents(RoomEvents&& events)
     return roomChanges;
 }
 
-void Room::Private::addHistoricalMessageEvents(RoomEvents&& events)
+std::pair<Room::Changes, Room::rev_iter_t> Room::Private::addHistoricalMessageEvents(RoomEvents&& events)
 {
-    const auto timelineSize = timeline.size();
 
     dropExtraneousEvents(events);
     if (events.empty())
-        return;
+        return { Change::None, historyEdge() };
 
+    const auto timelineSize = timeline.size();
     decryptIncomingEvents(events);
 
     QElapsedTimer et;
@@ -3184,12 +3191,9 @@ void Room::Private::addHistoricalMessageEvents(RoomEvents&& events)
     addRelations(from, historyEdge());
     Q_ASSERT(timeline.size() == timelineSize + insertedSize);
     if (insertedSize > 9 || et.nsecsElapsed() >= ProfilerMinNsecs)
-        qCDebug(PROFILER) << "Added" << insertedSize << "historical event(s) to"
-                          << q->objectName() << "in" << et;
-
-    changes |= updateStats(from, historyEdge());
-    if (changes)
-        postprocessChanges(changes);
+        qCDebug(PROFILER) << "Added" << insertedSize << "historical event(s) to" << q->objectName()
+                          << "in" << et;
+    return { changes, from };
 }
 
 void Room::Private::preprocessStateEvent(const RoomEvent& newEvent,

Quotient/room.h
@@ -167,7 +167,7 @@ class QUOTIENT_API Room : public QObject {
                    NOTIFY notificationCountChanged)
     Q_PROPERTY(EventStats partiallyReadStats READ partiallyReadStats NOTIFY partiallyReadStatsChanged)
     Q_PROPERTY(EventStats unreadStats READ unreadStats NOTIFY unreadStatsChanged)
-    Q_PROPERTY(bool allHistoryLoaded READ allHistoryLoaded NOTIFY addedMessages
+    Q_PROPERTY(bool allHistoryLoaded READ allHistoryLoaded NOTIFY allHistoryLoadedChanged
                    STORED false)
     Q_PROPERTY(QStringList tagNames READ tagNames NOTIFY tagsChanged)
     Q_PROPERTY(bool isFavourite READ isFavourite NOTIFY tagsChanged STORED false)
@@ -1049,6 +1049,7 @@ Q_SIGNALS:
     void unreadMessagesChanged(Quotient::Room* room);
     void partiallyReadStatsChanged();
     void unreadStatsChanged();
+    void allHistoryLoadedChanged();
 
     void accountDataAboutToChange(QString type);
     void accountDataChanged(QString type);

commit b636ce61bdd160d7e548acdb0e934cda893992ba

Fix building with Qt 5.15
Partially broken by #730 and not caught during the review; partially
broken in 4a93e124.

Quotient/connection.cpp
@@ -925,7 +925,7 @@ CreateRoomJob* Connection::createDirectChat(const QString& userId,
                                             const QString& topic,
                                             const QString& name)
 {
-    QList<CreateRoomJob::StateEvent> initialStateEvents;
+    QVector<CreateRoomJob::StateEvent> initialStateEvents;
 
     if (d->encryptDirectChats) {
         const auto encryptionContent = EncryptionEventContent(EncryptionType::MegolmV1AesSha2);

Quotient/room.cpp
@@ -2571,7 +2571,7 @@ void Room::Private::getPreviousContent(int limit, const QString& filter)
         if (!prevBatch)
             emit q->allHistoryLoadedChanged();
         changes |= updateStats(from, historyEdge());
-        if (changes != Change::None)
+        if (changes > 0)
             postprocessChanges(changes);
     });
     connect(eventsHistoryJob, &QObject::destroyed, q,