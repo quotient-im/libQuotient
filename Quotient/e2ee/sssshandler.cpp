456a78e4576dc0bdde157ad125604ec83dd46266
Tobias Fella <9750016+TobiasFella@users.noreply.github.com>
2024-05-12 19:21:46 +0200

    Apply suggestions from code review
    Co-authored-by: Alexey Rusakov <Kitsune-Ral@users.sf.net>

--- Quotient/e2ee/sssshandler.cpp
+++ Quotient/e2ee/sssshandler.cpp
@@ -217,7 +217,7 @@ void SSSSHandler::loadMegolmBackup(const QByteArray& megolmDecryptionKey)
                         static_cast<uint32_t>(backupData.firstMessageIndex));
                 }
             }
-            Q_EMIT finished();
+            emit finished();
         });
     });
 }
--- Quotient/events/keyverificationevent.h
+++ Quotient/events/keyverificationevent.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#include <Quotient/events/roomevent.h>
+#include "roomevent.h"
 
 namespace Quotient {
 
